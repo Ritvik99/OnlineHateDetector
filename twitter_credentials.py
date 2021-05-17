22757cc069b3614f5bd3d57b705a7fb0936dc01b
Ritvik99 <39411260+Ritvik99@users.noreply.github.com>
2021-05-17 15:42:56 +0530

    Add files via upload

---
+++ twitter_credentials.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 27 09:21:05 2019
+
+@author: ritvi
+"""
+accessToken = "2391017317-cZwr5MYS3s5eAcirTbE847BjbY6aROadNALdKOm"
+accessTokenSecret = "PjqfL9gd0y01JlVIZpnC0I5W2i4Lzok26InK328E2P5Ti"
+consumerKey = "0MoO7hi2SqsauyIyym4DJLuo4"
+consumerSecret = "3W6DXLHN8R2AiB6FG5kk83Fmiz4UtwdTrz15De2BnhYwyZZV63"
